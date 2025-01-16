b033f6798e2514adcce1efdacb6bf5cae30e699c
jrsangst <jrsangst@edu.uwaterloo.ca>
2025-01-16 13:29:17 -0800

    fixing package dep. issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import {HashRouter, Routes, Route, Link} from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => <div>
+  <h1> Welcome to my Personal Website! 🥳 </h1>
+  <h6>I began converting my personal website from pure HTML/CSS to React on January 16th, 2025</h6>
+  <h6>  If you're a lululemon recruiter, please know the timing of this transfer was poorly timed with my application</h6>
+  <h4>I hope to hear from you soon!</h4>
+
+  <h4>- Julia </h4> 
+</div>;
+const About = () => <div>About Page</div>;
+const Contact = () => <div>Contact Page</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <HashRouter>
+          <Navbar>
+            <Navbar.Brand
+              className="mx-3"
+              style={{fontFamily: "Consolas, monaco, monospace", color: "#007200"}}> Julia Sangster </Navbar.Brand>
+            <Navbar.Collapse>
+              <Nav.Link as={Link} to="/" className="mx-3">Home</Nav.Link>
+              <Nav.Link as={Link} to="/about" className="mx-3">About</Nav.Link>
+              <Nav.Link as={Link} to="/contact" className="mx-3">Contact</Nav.Link>
+            </Navbar.Collapse>
+          </Navbar>
+
+
+          <Routes>
+            <Route path="/" element = {<Home />} />
+            <Route path="/about" element = {<About />} />
+            <Route path="/contact" element = {<Contact />} />
+          </Routes>
+
+      </HashRouter>
+
     </div>
   );
 }
+
 
 export default App;
